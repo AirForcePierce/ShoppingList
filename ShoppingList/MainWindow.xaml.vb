71589b657dd3c8d292616d60895d2587b028cf93
AirForcePierce <jontae.pierce@gmail.com>
2016-02-04 09:47:52 -0500

    Button working adding values

--- ShoppingList/MainWindow.xaml.vb
+++ ShoppingList/MainWindow.xaml.vb
@@ -1,3 +1,12 @@
 ﻿Class MainWindow 
 
+    Private Sub AddValue_Click(sender As Object, e As RoutedEventArgs) Handles AddValue.Click
+        Dim newValue As String = InputBox.Text
+        Output(newValue)
+    End Sub
+
+    Sub Output(Value As String)
+        TextOutput.Text += Value + vbCrLf
+    End Sub
+
 End Class

18e9ac58be0e337f623adfc2cb5a707415a723cc
AirForcePierce <jontae.pierce@gmail.com>
2016-02-04 10:20:02 -0500

    Enter button function added

--- ShoppingList/MainWindow.xaml.vb
+++ ShoppingList/MainWindow.xaml.vb
@@ -3,6 +3,15 @@
     Private Sub AddValue_Click(sender As Object, e As RoutedEventArgs) Handles AddValue.Click
         Dim newValue As String = InputBox.Text
         Output(newValue)
+        InputBox.Clear()
+    End Sub
+
+    Private Sub OnKeyDownHandler(ByVal sender As Object, ByVal e As KeyEventArgs)
+        If (e.Key = Key.Return) Then
+            Dim newValue As String = InputBox.Text
+            Output(newValue)
+            InputBox.Clear()
+        End If
     End Sub
 
     Sub Output(Value As String)
